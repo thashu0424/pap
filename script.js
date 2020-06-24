40b21a9ad63dcef001643887d30b70d97787d336
thashu0424 <52147960+thashu0424@users.noreply.github.com>
2020-06-24 11:35:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
+const lear1 = document.querySelector('.learn1>button');
+const lear2 = document.querySelector('.learn2>button');
+const lear3 = document.querySelector('.learn3>button');
+const first = document.querySelector('.first-li1');
+const second = document.querySelector('.first-li2');
+const third = document.querySelector('.first-li3');
 var checkbox = document.querySelector('input');
-const bod = document.querySelector('body');
-const sqbox = document.querySelectorAll('header,.box1,.box2,.box3,.box4,.sub1,.sub2,.sub3,.sub4,.sub5,.sub6,.sub7,.sub8');
-const tex = document.querySelectorAll('h1,.number,.vi');
-const textt = document.querySelectorAll('.heade>h1,.mele>p');
+console.log(checkbox);
 
 checkbox.addEventListener('change', function() {
-        if (checkbox.checked == true) {
-            bod.style.backgroundColor = 'white';
-            sqbox.forEach(function(jai) {
-                jai.style.backgroundColor = "hsl(227, 47%, 96%)";
-            });
-            tex.forEach(function(kai) {
-                kai.style.color = "hsl(230, 17%, 14%)";
-            });
-            textt.forEach(function(mai) {
-                mai.style.color = "hsl(228, 12%, 44%)";
-            });
+    if (checkbox.checked == true) {
+        first.textContent = '$19.99';
+        second.textContent = '$24.99';
+        third.textContent = '$39.99';
+        second.style.color = "white";
 
-        } else {
-            bod.style.backgroundColor = '#1e202a';
 
-            sqbox.forEach(function(jai) {
-                jai.style.backgroundColor = "hsl(228, 28%, 20%)";
-            });
-            tex.forEach(function(kai) {
-                kai.style.color = "hsl(0, 0%, 100%)";
-            });
-        }
-
+    } else {
+        first.textContent = '$199.99';
+        second.textContent = '$249.99';
+        third.textContent = '$399.99';
+        second.style.color = "white";
+        lear1.style.backgroundColor = "hsl(237, 63%, 64%)";
+        lear2.style.backgroundColor = "white";
+        lear3.style.backgroundColor = "hsl(237, 63%, 64%)";
+        lear1.style.color = "white";
+        lear2.style.color = "hsl(237, 63%, 64%)";
+        lear3.style.color = "white";
     }
-
-)
+});
